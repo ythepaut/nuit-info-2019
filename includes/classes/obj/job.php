862e8d0c1b9b30be23eb59bf10fc97e9596291fa
LucCha42 <55276692+LucCha42@users.noreply.github.com>
2019-12-05 23:31:09 +0100

    Objet job (plus Announce)

--- includes/classes/obj/job.php
+++ includes/classes/obj/job.php
@@ -4,21 +4,25 @@ include_once("./includes/classes/obj/database.php");
 
 
 
-class Announce {
+class Job {
     private $id;
     private $title;
     private $description;
     private $categorie;
+    private $location;
+    private $date;
     private $owner;
 
     public function __construct($id) {
         $this->id = $id;
 
-        $queryRes = DatabaseHandler::executeQuery("SELECT * FROM nuitinfo_announces WHERE id = ?", array($id));
+        $queryRes = DatabaseHandler::executeQuery("SELECT * FROM nuitinfo_jobs WHERE id = ?", array($id));
 
         $this->title = $queryRes["title"];
         $this->description = $queryRes["description"];
         $this->categorie = $queryRes["categorie"];
+        $this->location = $queryRes["location"];
+        $this->date = $queryRes["date"];
         $this->owner = $queryRes["owner"];
     }
 
