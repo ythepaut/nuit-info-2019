fdaa9de53099163b5a60711150063955c469a652
Romain <romain.theodet@hotmail.fr>
2019-12-05 20:23:31 +0100

    Pages de base

--- includes/classes/view.php
+++ includes/classes/view.php
@@ -1,5 +1,22 @@
 <?php
 
+#Header
+include("./includes/pages/header.php");
 
+$page = (isset($_GET['page'])) ? $_GET['page'] : "";
+
+switch ($page) {
+    #Pages "vitrine"
+    case "":
+    case "index":
+    case "accueil":
+        include("./includes/pages/accueil.php");
+        break;
+
+    case "404":
+    default:
+        include("./includes/pages/404.php");
+        break;
+}
 
 ?>
