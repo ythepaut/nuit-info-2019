8f88a3b2f15ad4c00f109be57ecfc95ab1e78bff
Yohann THEPAUT <comptes@ythepaut.com>
2019-12-06 04:46:51 +0100

    Fct login + enregistrement

--- includes/classes/actions.php
+++ includes/classes/actions.php
@@ -11,6 +11,9 @@ switch ($action) {
         session_destroy();
         header("Location: /");
         break;
+    case "login":
+        header("Location: /");
+        break;
     case "blind-mode":
         $_SESSION["blindMode"] = !$_SESSION["blindMode"];
         header("Location: /");
@@ -22,6 +25,163 @@ switch ($action) {
         throw new Exception("ERROR_MISSING_ACTION");
         break;
 }
+
+
+
+
+
+/**
+ * Connexion de l'utilisateur : Methode e-mail + mot de passe
+ * (Formulaire AJAX)
+ *
+ * @param string            $email              -   Adresse e-mail de l'utilisateur
+ * @param string            $passwd             -   Mot de passe de l'utilisateur
+ * @param mysqlconnection   $connection         -   Connexion BDD effectuée dans le fichier config-db.php
+ *
+ * @return string
+ */
+function login($email, $passwd, $connection) {
+    $result = "ERROR_UNKNOWN#Une erreur est survenue.";
+
+    //Verification des champs
+    if (isset($email, $passwd, $connection) && $email != "" && $passwd != "") {
+
+        //Recuperation des données
+        $query = $connection->prepare("SELECT * FROM nuitinfo_users WHERE email = ?");
+        $query->bind_param("s", $email);
+        $query->execute();
+        $result = $query->get_result();
+        $query->close();
+        $userData = $result->fetch_assoc();
+
+        //Identifiants correct ?
+        if (isset($userData['id']) && $userData['id'] != null && password_verify(hash('sha512', hash('sha512', $passwd . $userData['salt'])), $userData['password'])) {
+
+            //Verification du compte
+            if ($userData['status'] == "ALIVE") {
+
+                #Attribution des données de session
+                $_SESSION['Data'] = $userData;
+                $_SESSION['LoggedIn'] = true;
+
+                $result = "SUCCESS#Bienvenue " . $_SESSION['Data']['username'] . "#/espace-utilisateur/accueil";
+
+            } else {
+
+                switch ($userData['status']) {
+                    case "SUSPENDED":
+                        $result = "ERROR_ACCOUNT_SUSPENDED#Connexion impossible : Ce compte est suspendu.";
+                        break;
+                    default:
+                        $result = "ERROR_INVALID_ACCESSLEVEL#Connexion impossible : Niveau d'accès insuffisant.";
+                        break;
+                }
+
+            }
+
+        } else {
+            $result = "ERROR_INVALID_CREDENTIALS#Identifiants de connexion invalides.";
+        }
+
+    } else {
+        $result = "ERROR_MISSING_FIELDS#Veuillez remplir tous les champs.";
+    }
+
+    return $result . "#<script>window.href.location = '/';</script>";
+}
+
+
+
+/**
+ * Enregistrement d'un nouvel utilisateur
+ * (Formulaire AJAX)
+ *
+ * @param string            $username           -   Nom d'utilisateur
+ * @param string            $email              -   Adresse e-mail de l'utilisateur
+ * @param string            $passwd             -   Mot de passe de l'utilisateur
+ * @param string            $passwd2            -   Mot de passe de l'utilisateur (confirmation)
+ * @param mysqlconnection   $connection         -   Connexion BDD effectuée dans le fichier config-db.php
+ *
+ * @return string
+ */
+function register($username, $email, $passwd, $passwd2, $connection) {
+    $result = "ERROR_UNKNOWN#Une erreur est survenue.";
+
+    //Verification des champs
+    if (isset($username, $email, $passwd, $passwd2, $connection) && $email != "" && $passwd != "" && $passwd2 != "" && $username != "") {
+
+        if (strlen($username) <= 16 && strlen($username) >= 3) {
+
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+                if (strlen($passwd) >= 8 && preg_match("#[0-9]+#", $passwd) && preg_match("#[a-zA-Z]+#", $passwd)) {
+
+                    if ($passwd == $passwd2) {
+
+                        //Verification données e-mail
+                        $query = $connection->prepare("SELECT * FROM nuitinfo_users WHERE email = ?");
+                        $query->bind_param("s", $email);
+                        $query->execute();
+                        $result = $query->get_result();
+                        $query->close();
+                        $userData = $result->fetch_assoc();
+
+                        if ($userData['id'] == "") {
+
+                            //Verification données nom d'utilisateur
+                            $query = $connection->prepare("SELECT * FROM nuitinfo_users WHERE username = ?");
+                            $query->bind_param("s", $username);
+                            $query->execute();
+                            $result = $query->get_result();
+                            $query->close();
+                            $userData = $result->fetch_assoc();
+
+                            if ($userData['id'] == "") {
+
+                                $salt = randomString(16);
+                                $password_salted_hashed = password_hash(hash('sha512', $passwd . $salt), PASSWORD_DEFAULT, ['cost' => 12]);
+                                $status = "ALIVE";
+
+                                $query = $connection->prepare("INSERT INTO nuitinfo_users (email, username, password, salt, status) VALUES (?,?,?,?,?)");
+                                $query->bind_param("sssss", $email, $username, $password_salted_hashed, $salt, $status);
+                                $query->execute();
+                                $query->close();
+
+                                $result = "SUCCESS#Compte créé avec succès.#/";
+
+                            } else {
+                                $result = "ERROR_USED_USERNAME#Ce nom d'utilisateur est déjà utilisé.";
+                            }
+
+                        } else {
+                            $result = "ERROR_USED_EMAIL#Cette adresse e-mail est déjà utilisée.";
+                        }
+
+                    } else {
+                        $result = "ERROR_INVALID_PASSWD2#Les deux mots de passe doivent correspondre.";
+                    }
+
+                } else {
+                    $result = "ERROR_INVALID_PASSWD#Votre mot de passe doit faire au moins 8 caractères, contenir au moins une lettre et un chiffre.";
+                }
+
+            } else {
+                $result = "ERROR_INVALID_EMAIL#Veuillez saisir une adresse e-mail valide.";
+            }
+
+        } else {
+            $result = "ERROR_INVALID_USERNAME#Votre nom d'utilisateur doit faire entre 3 et 16 caractères.";
+        }
+
+    } else {
+        $result = "ERROR_MISSING_FIELDS#Veuillez remplir tous les champs.";
+    }
+
+    return $result . "#<script>window.href.location = '/';</script>";
+}
+
+
+
 
 
 
