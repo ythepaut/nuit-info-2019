34d7ad242d64c8c8fa32ff92b93be7ec546af266
445654 <50819147+445654@users.noreply.github.com>
2019-12-06 03:36:23 +0100

    Ajout fonction ajoutService

--- includes/classes/actions.php
+++ includes/classes/actions.php
@@ -15,9 +15,32 @@ switch ($action) {
         $_SESSION["blindMode"] = !$_SESSION["blindMode"];
         header("Location: /");
         break;
+    case "ajout-service":
+        die(ajoutService($_POST['title'], $_POST['description'], $_POST['categorie'], $_POST['location'], $connection));
+        break;
     default:
         throw new Exception("ERROR_MISSING_ACTION");
         break;
+}
+/**
+ * Foncton qui ajoute un service dans la bdd
+ * @param string            $title          - Le titre du service
+ * @param string            $description    - La description du service
+ * @param string            $categorie      - La catégorie du service
+ * @param array             $location       - La localisation du service
+ * @param integer           $ownerId        - L'identifiant du demandeur du service
+ * 
+ * @return void
+ */
+function ajoutService($title, $description, $categorie, $location, $connection) {
+    if (isset($title, $description, $categorie, $location, $ownerId) && $location != {} && $title != "" && $description != "" && $categorie != "") {
+        if (categorieExist($categorie)) {
+            $query = $connection->prepare("INSERT INTO nuitinfo_services (title, description, categorie, location, date, owner) VALUES (?, ?, ?, ?, ?, ?) ");
+            $query->bind_param("ssssii", $title, $description, $categorie, $location, time(), $_SESSION['Data']['id']);
+            $query->execute();
+            $query->close();
+        }
+    }
 }
 
 ?>
